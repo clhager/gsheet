e8f323c339807c6d7067866273bf1719cbb8efc0
clhager <clhager@berkeley.edu>
2017-10-03 22:32:48 -0700

    Added to README

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
 var GSheet = require('./googlesheets');
 var async   = require('async');
 
-var url = 'https://script.google.com/macros/s/AKfycbzei4DTwpNV1UFgD7uVniXLOrle489VL1V4UM0X6TxF5vgmQOc/exec';
-var ID = '1bVaReDTArbcS_aBlSk4c2qPA1slthSKc3Uq3H0WnwqM';
-var name = 'Data';
+var url = 'Google-App-Scripts-Exec-URL';
+var ID = 'Spreadsheet-ID';
+var name = 'Sheet-Name';
 
 var sheet = new GSheet(url, ID, name);
 //sheet.setOpts(ID, name);
 
-sheet.setRow(0, ["BS", "Pink", "hi", "yo"]);
-sheet.setRow(1, ["School", "Pink", "hi", "ho"]);
-sheet.setRow(2, ["Stadium", "Pink", "hi", "2"]);
-sheet.setRow(3, ["Stadium", "Pink", "hasdfasdfi", "3"]);
+sheet.setRow(0, ["a", "b", "c", "d"]);
+sheet.setRow(1, ["1", "2", "3", "4"]);
+sheet.setRow(2, ["A", "B", "C", "D"]);
 
 async.series([
 	function(step) {
